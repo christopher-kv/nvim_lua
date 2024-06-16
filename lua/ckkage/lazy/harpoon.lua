32eabdc208bc72732db910e11a940e442b4f61a9
christopher-kv <christopherkarlon@gmail.com>
2024-06-16 02:35:16 +0000

    corrected treesiter config error

--- lua/ckkage/lazy/harpoon.lua
+++ lua/ckkage/lazy/harpoon.lua
@@ -1,21 +1,3 @@
--- return {
--- 	"ThePrimeagen/harpoon",
--- 	branch = "harpoon2",
--- 	dependencies = {"nvim-lua/plenary.nvim"},
---     init = function()
---         local harpoon = require("harpoon")
---         harpoonr:setup()
---     end,
---     keys = {
---         { "<leader>a", {function() harpoon:list():append() end, mode="n"} },
---         { '<C-e>', {function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, mode="n"} },
---         { "<leader>j", {function() harpoon:list():select(1) end, mode="n"} },
---         { "<leader>k", {function() harpoon:list():select(2) end, mode="n"} },
---         { "<leader>l", {function() harpoon:list():select(3) end, mode="n"} },
---         { "<leader>i", {function() harpoon:list():select(4) end, mode="n"} },
---     },
--- }
-
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
@@ -37,7 +19,7 @@ return {
         desc = "Harpoon File",
       },
       {
-        "<leader>h",
+        "<leader>e",
         function()
           local harpoon = require("harpoon")
           harpoon.ui:toggle_quick_menu(harpoon:list())
